2a8c5d82ea46c153ce6821160f4f00a886c62dd3
mareksl <marekslad@hotmail.com>
2018-10-04 16:45:39 +0200

    add statistics by criteria

--- src/actions/statistics.actions.js
+++ src/actions/statistics.actions.js
@@ -18,11 +18,46 @@ const calculatePercentageDone = funds => {
   return amountDone / funds.length;
 };
 
+const calculateStatsByProperty = (funds, property) => {
+  return funds.reduce((result, fund) => {
+    if (!(fund[property] in result)) {
+      result[fund[property]] = {
+        totalCount: 0,
+        doneCount: 0,
+        percentageDone: 0
+      };
+    }
+
+    result[fund[property]].totalCount += 1;
+    result[fund[property]].doneCount = checkFundStatus(fund)
+      ? result[fund[property]].doneCount + 1
+      : result[fund[property]].doneCount;
+    result[fund[property]].percentageDone =
+      result[fund[property]].doneCount / result[fund[property]].totalCount;
+    return result;
+  }, {});
+};
+
+const calculateStatsByRank = funds => {
+  return calculateStatsByProperty(funds, 'highestRank');
+};
+
+const calculateStatsByDepartment = funds => {
+  return calculateStatsByProperty(funds, 'department');
+};
+
+const calculateStatsByAssignee = funds => {
+  return calculateStatsByProperty(funds, 'fundOwner');
+};
+
 const calculateStatistics = funds => {
   return {
     totalCount: funds.length,
     doneCount: calculateAmountDone(funds),
-    percentageDone: calculatePercentageDone(funds)
+    percentageDone: calculatePercentageDone(funds),
+    statsByRank: calculateStatsByRank(funds),
+    statsByDepartment: calculateStatsByDepartment(funds),
+    statsByAssignee: calculateStatsByAssignee(funds)
   };
 };
 
